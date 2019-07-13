c6c23a2308de403cdc5cf9da2d26297b7eabeb65
Your Name <you@example.com>
2019-07-13 00:08:57 -0700

    driver-api-routes

--- routes/driver-api-routes.js
+++ routes/driver-api-routes.js
@@ -1,24 +1,59 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+module.exports = function (app) {
+    // Get all drivers rides
+    app.get("/api/rides", function (req, res) {
+        var query = {};
+        if (req.query.user_id) {
+            query.userId = req.query.userTest_id;
+        }
+        db.driverRide.findAll({
+            where: query,
+        }).then(function (dbRides) {
+            res.json(dbRides);
+        });
     });
-  });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+    // Get route for retrieving a single ride
+    app.get("/api/rides/:id", function (req, res) {
+        db.driverRide.findOne({
+            where: {
+                id: req.params.id
+            },
+        }).then(function (dbRides) {
+            res.json(dbRides);
+        });
     });
-  });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+
+    // Create a new ride
+    app.post("/api/rides", function (req, res) {
+        db.driverRide.create(req.body).then(function (dbRides) {
+            res.json(dbRides);
+        });
     });
-  });
+
+    // Delete a ride by id
+    app.delete("/api/rides/:id", function (req, res) {
+        db.driverRide.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function (dbRides) {
+            res.json(dbRides);
+        });
+    });
+
+    // PUT route for updating rides
+    app.put("/api/rides", function (req, res) {
+        db.driverRide.update(
+            req.body, {
+                where: {
+                    id: req.body.id
+                }
+            }).then(function (dbRides) {
+            res.json(dbRides);
+        });
+    });
 };
+
