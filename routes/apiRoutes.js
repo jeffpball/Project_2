185c7f4be1552b9afada828e058d414b6d6b5d98
ReactProX <gaviniosso@gmail.com>
2019-07-19 19:14:14 -0700

    rebuilt functions around new developemnt paradigm

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,18 +5,11 @@ var riderLogic = require("./riderLogic")
 
 module.exports = function (app) {
   app.post("/api/rides/search/", function (req, res) {
-    console.log(req.body.startTime);
     db.driverRide.findAll({
     }).then(function(response){
       var ridePosTime = riderLogic.timeMath(response, req.body.startTime, req.body.endTime);
+      riderLogic.compZips(req.body.riderStartZip, req.body.riderEndZip, ridePosTime)
       res.json(ridePosTime);
-      for(var i = 0; i < ridePosTime.length; i++){
-        console.log(riderLogic.addRG(req.body.riderStart), riderLogic.addRG(req.body.riderEnd), riderLogic.addRG(ridePosTime[i].pick_up_address), riderLogic.addRG(ridePosTime[i].drop_off_address));
-        riderLogic.distanceCall(riderLogic.addRG(req.body.riderStart), riderLogic.addRG(req.body.riderEnd), riderLogic.addRG(ridePosTime[i].pick_up_address), riderLogic.addRG(ridePosTime[i].drop_off_address))
-      }
-      
-    })
-    
+    })    
   });
-
 };
